068cbe68c58b24cc3f9a8008643a431babaf7aa0
Marcelo Pasut <pasutmarcelo@gmail.com>
2019-01-29 01:42:44 -0300

    comento el problema con los stages

--- scripts/deploy_complete.sh
+++ scripts/deploy_complete.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -
+# Cuando se corre para un nuevo ENV hay que eliminar lo relacionado a la API key, para que pueda enerar bien el stage
 sam build --parameter-overrides ParameterKey=InvertirOnlinePassword,ParameterValue=$INVERTIRONLINE_PASSWORD,ParameterKey=InvertirOnlineUsername,ParameterValue=$INVERTIRONLINE_USERNAME,ParameterKey=targetStage,ParameterValue=$ENV --template ./template.yaml --build-dir ./build -u
 sam package --template-file ./build/template.yaml --output-template-file ./build/packaged-template.yaml --s3-bucket trading-serverless
 sam deploy --parameter-overrides InvertirOnlinePassword=$INVERTIRONLINE_PASSWORD InvertirOnlineUsername=$INVERTIRONLINE_USERNAME targetStage=$ENV --template-file ./build/packaged-template.yaml --stack-name trading-serverless
